57b3f6f5b3142bd9794850dd11a7fedbd321b777
moerwald <andreas.mewald@gmail.com>
2020-01-18 19:35:45 +0100

    - Added heroes list view

---
+++ heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { HEROES } from './../mock-hereos';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero : Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero:Hero){
+    this.selectedHero = hero;
+
+  }
+
+}
